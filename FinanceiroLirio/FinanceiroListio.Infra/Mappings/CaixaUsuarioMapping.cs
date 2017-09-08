6074d22c214ca3d4486f770012e58b2a05399a1d
Anderson Pereira <anderson.rasec@outlook.com>
2017-09-07 23:42:33 -0300

    Criacção dos Mappings de Caixa e CaixaUsuario
    Criacção dos Mappings de Caixa e CaixaUsuario

---
+++ FinanceiroLirio/FinanceiroListio.Infra/Mappings/CaixaUsuarioMapping.cs
@@ -0,0 +1,28 @@
+﻿using FinanceiroLirio.Entidades;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+
+namespace FinanceiroListio.Infra.Mappings
+{
+    class CaixaUsuarioMapping : EntityTypeConfiguration<CaixaUsuario>
+    {
+        public CaixaUsuarioMapping()
+        {
+            HasKey(c => c.IdCaixaUsuario);
+
+            Property(c => c.IdCaixaUsuario)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            Property(c => c.DataInclusao)
+                .IsRequired();
+
+            HasRequired(c => c.Caixa)
+                .WithMany()
+                .HasForeignKey(c => c.IdCaixa);
+
+            HasRequired(c => c.Usuario)
+                .WithMany()
+                .HasForeignKey(c => c.IdUsuario);
+        }
+    }
+}
