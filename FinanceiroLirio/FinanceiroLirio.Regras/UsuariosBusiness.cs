4f1ced6d563d5a6d2db0aabe7739854e31b4c618
ivossjunior <ivossjunior@gmail.com>
2017-09-11 20:05:26 -0300

    Criptografia de dados e adição da camada de negócio
    Dois projetos adicionados, um de utilitários, onde está a classe de
    criptografia de dados utilizando o md5  e um projeto de regras de
    negócio, inicialmente contendo apenas as regras de usuário.

---
+++ FinanceiroLirio/FinanceiroLirio.Regras/UsuariosBusiness.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinanceiroLirio.Entidades;
+using FinanceiroLirio.Infra.Persistence;
+using FinanceiroLirio.Util;
+
+namespace FinanceiroLirio.Regras
+{
+    public class UsuariosBusiness
+    {
+        public Usuario RealizarLogin(string login, string senha)
+        {
+            Usuario u = null;
+            try
+            {
+                if (login == null || senha == null)
+                {
+                    throw new Exception("Login e/ou senha inválidos");
+                }
+
+                UsuarioRepository ur = new UsuarioRepository();
+
+                u = ur.FindByLoginSenha(login, Criptografia.CriptografarMD5(senha));
+
+                if(u == null)
+                {
+                    throw new Exception("Login e/ou senha incorretos. Tente novamente.");
+                }
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+            return u;
+        }
+    }
+}
