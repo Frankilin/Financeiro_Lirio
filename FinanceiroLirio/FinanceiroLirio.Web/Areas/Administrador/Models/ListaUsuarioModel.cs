ea29440ac5a26ce7359d8211ce8ce55571e0bc3b
Anderson Pereira <anderson.rasec@outlook.com>
2017-11-24 00:57:55 -0300

    Retirada a coluna senha da listagem de usuários.

--- FinanceiroLirio/FinanceiroLirio.Web/Areas/Administrador/Models/ListaUsuarioModel.cs
+++ FinanceiroLirio/FinanceiroLirio.Web/Areas/Administrador/Models/ListaUsuarioModel.cs
@@ -11,7 +11,6 @@ namespace FinanceiroLirio.Web
     {
         public string NomeCompleto { get; set; }
         public string Login { get; set; }
-        public string Senha { get; set; }
         public string Email { get; set; }
         public DateTime DataCadastro { get; set; }
 
